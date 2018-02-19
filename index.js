2862c1036aec769743bcbee3a93c81f1c196cfa7
Mariel Capulii <mariel.capulli@gmail.com>
2018-02-19 19:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
 }
+
+//question 4 
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!";
+}
